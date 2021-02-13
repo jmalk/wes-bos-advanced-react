b15693c9eafc03f63c328fbf58e6b0f349cce2cf
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-02-13 18:25:37 +0000

    Add shared templates for all pages

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,11 @@
+// Wes said it was ok...
+/* eslint-disable react/jsx-props-no-spreading */
+import Page from '../components/Page';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
