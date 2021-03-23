adda06ce1a613a631d851289cbe642cee4116cb5
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-03-23 17:24:58 +0000

    08: Show loading bar on route change

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,22 @@
 // Wes said it was ok...
 /* eslint-disable react/jsx-props-no-spreading */
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+
+Router.events.on('routeChangeError', () => {
+  NProgress.done();
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
