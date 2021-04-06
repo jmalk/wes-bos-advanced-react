85080f3a9bec2dc7fd21c7e449f886e6ab994c43
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-04-06 15:45:27 +0100

    15: Create a Product data type

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 } from '@keystone-next/keystone/session';
 
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -45,6 +46,7 @@ export default withAuth(
     },
     lists: createSchema({
       User,
+      Product,
     }),
     ui: {
       // Only show db admin UI for people who pass this test
