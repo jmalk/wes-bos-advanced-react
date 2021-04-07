2409e5bfcd626c4f41f55a6b57f169b7a4798f4a
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-04-07 18:18:07 +0100

    18: Wire up seed data command

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -9,6 +9,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -43,7 +44,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       User,
