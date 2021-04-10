833c6edee97b0dac6545af4c527254c4e8b603fc
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-04-10 12:35:49 +0100

    19: Set up Apollo Client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,8 +1,10 @@
 // Wes said it was ok...
 /* eslint-disable react/jsx-props-no-spreading */
+import { ApolloProvider } from '@apollo/client';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 import '../components/styles/nprogress.css';
 
@@ -18,10 +20,24 @@ Router.events.on('routeChangeError', () => {
   NProgress.done();
 });
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
